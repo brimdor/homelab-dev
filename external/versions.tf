bf1997f486173c30a95df9cd1820b23420389edf
brimdor <chrisnelsonx@gmail.com>
2024-02-19 19:28:49 +0000

    latest build for dev

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "brimdor"
+    organization = "brimdor-dev"
 
     workspaces {
       name = "homelab-external"
