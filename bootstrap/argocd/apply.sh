8c69618a818e66ea5a4209b7a45b4b2957e0d7bb
brimdor <chrisnelsonx@gmail.com>
2024-02-25 16:40:49 -0600

    latest clean up of ArgoCD CRDs change

--- bootstrap/argocd/apply.sh
+++ bootstrap/argocd/apply.sh
@@ -5,17 +5,8 @@ VALUES="values.yaml"
 kubectl get ingress argocd-server --namespace argocd \
     || VALUES="values-seed.yaml"
 
+echo ""
+echo "<<<<<< ArgoCD Pods and Services >>>>>>"
 kubectl get pods --namespace argocd
 kubectl get services --namespace argocd
-
-# helm template \
-#     --dependency-update \
-#     --include-crds \
-#     --namespace argocd \
-#     --values "${VALUES}" \
-#     argocd . \
-#     | kubectl apply -n argocd -f -
-
-# kubectl -n argocd wait --timeout=60s --for condition=Established \
-#        crd/applications.argoproj.io \
-#        crd/applicationsets.argoproj.io
+echo ""
