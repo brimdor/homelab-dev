8172f03906392834283ce884a13aa7c957022608
brimdor <chrisnelsonx@gmail.com>
2024-03-09 22:17:48 -0600

    Update versions.tf

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "brimdor2"
+    organization = "brimdor-dev"
 
     workspaces {
       name = "homelab-external"
