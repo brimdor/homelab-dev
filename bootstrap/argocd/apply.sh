78eb130a91cd294148d8efbd6767b461bcf3c012
brimdor <chrisnelsonx@gmail.com>
2024-03-10 19:10:32 -0500

    Update apply.sh

--- bootstrap/argocd/apply.sh
+++ bootstrap/argocd/apply.sh
@@ -7,15 +7,3 @@ kubectl get ingress argocd-server --namespace argocd ||
 
 kubectl get pods --namespace argocd
 kubectl get services --namespace argocd
-
-# helm template \
-#     --dependency-update \
-#     --include-crds \
-#     --namespace argocd \
-#     --values "${VALUES}" \
-#     argocd . |
-#     kubectl apply -n argocd -f -
-
-# kubectl -n argocd wait --timeout=60s --for condition=Established \
-#        crd/applications.argoproj.io \
-#        crd/applicationsets.argoproj.io
